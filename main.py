55a678b9272217ba84b4b0be36a89a8118f7bfc8
manhpv <manhterry93@gmail.com>
2022-01-27 13:18:55 +0700

    Update full features

--- main.py
+++ main.py
@@ -3,44 +3,51 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import datetime
+import logging
+
 import crontab
 import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    logging.info(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+    logging.info('dir: ' + dir_path)
 
 
 def setup_cron_job():
-    print('setup cron job')
+    logging.info('setup cron job')
     temp_cron = crontab.CronTab(user='manhpv')
-    job = temp_cron.new(command='python3 /home/manhpv/BackendProjects/CronJob/write_date.py', comment="test_job")
-    job.minute.every(1)
+    py_file = dir_path + '/psu.py'
+    job = temp_cron.new(command='python3 {}'.format(py_file), comment="psu_job")
+    job.hour.every(1)
     temp_cron.write()
-    print(job)
 
 
 def disable_job():
-    print('disable job')
+    logging.info('disable job')
     temp_cron = crontab.CronTab(user='manhpv')
     for job in temp_cron:
-        if job.comment == 'test_job':
-            print(job)
+        if job.comment == 'psu_job':
+            logging.info(job)
             temp_cron.remove(job)
             temp_cron.write()
 
 
 def check_job():
-    print('check job')
+    logging.info('check job')
     my_crons = crontab.CronTab(user='manhpv')
     for job in my_crons:
-        print(job)
-        print(job.is_valid())
+        logging.info(job)
+        logging.info(job.is_valid())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    logging.basicConfig(filename='cron_job.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
     print_hi('PyCharm')
     args = sys.argv
     # setup_cron_job()
--- psu.py
+++ psu.py
@@ -1,25 +1,124 @@
 import psutil
 import sys
+import subprocess
+import datetime
+import pymongo
+import logging
+import json
 
-RAM_THRESHOLD = 3221225472  # 3GB
+RAM_THRESHOLD = 1073741824  # 1GB
+# RAM_THRESHOLD = 10737418240  # 10GB
+
+
+config = {}
+
+
+def check_process():
+    """
+    Check process to logging the mongodb status:
+    * Total instance count
+    * Total Memory consume
+    """
+    listOfProcObjects = []
+    mongo_count = 0
+    mongo_total_size = 0
+    logging.info('------Logging mongo processes memory---')
+    for proc in psutil.process_iter():
+        try:
+            # Fetch process details as dict
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+            pinfo['vms'] = proc.memory_info().vms
+            # Append dict to list
+            if pinfo['name'] == 'mongod':
+                mongo_count += 1
+                mongo_total_size += pinfo['vms']
+                listOfProcObjects.append(pinfo)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+        # Sort list of dict by key vms i.e. memory usage
+
+    # listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['vms'], reverse=True)
+
+    # with open('processes.txt', 'w') as outFile:
+    #     # Iterate over all running processes
+    #     for item in listOfProcObjects:
+    #         outFile.write(json.dumps(item) + '\n')
+    #     outFile.close()
+    log_data = {
+        "mongo_count": mongo_count,
+        "mongo_total_size": mongo_total_size
+    }
+    logging.info("mongo processes detail: {}".format(log_data))
+    check_mongo_status()
 
 
 def check_memory():
+    logging.info("\n--------Checking Memory---------")
     print('virtual memory: {}'.format(psutil.virtual_memory().available))
     # Check available RAM in Bytes
     virtual_memory = psutil.virtual_memory().available
-    if virtual_memory >= RAM_THRESHOLD:
-        # Reset mongo db from here
-        pass
-    print('swap memory: {}'.format(psutil.swap_memory()))
+    logging.info(
+        "Available RAM: {} MB/{} MB".format(bytes_to_MB(virtual_memory), bytes_to_MB(psutil.virtual_memory().total)))
+    if virtual_memory < RAM_THRESHOLD:
+        logging.info("RAM below threshold ({} MB)".format(bytes_to_MB(RAM_THRESHOLD)))
+        check_process()
+    else:
+        logging.info("No need to reset mongo docker\n")
+
+
+def check_mongo_status():
+    """
+    Check status of mongo db, if current mongo is Primary in replicaset,
+    reset mongo db docker
+    """
+    logging.info("Checking mongo status...")
+    if not load_config():
+        return
+    try:
+        client = pymongo.MongoClient(config['mongo_uri'])
+        client_role = client.read_preference.name
+        if client_role.lower() == "primary":
+            logging.info("Mongo client is PRIMARY  ==> Reset")
+            client.close()
+            reset_mongo_docker()
+    except Exception as e:
+        logging.info("Check mongo status failed with error")
+        logging.error(e)
+
+
+def reset_mongo_docker():
+    try:
+        logging.info("Resetting1 mongo docker...")
+        subprocess.run(["docker-compose", "-f",
+                        config['docker-compose_file_path'], "restart",
+                        config['service_name']])
+    except Exception as e:
+        logging.info("Error in reset mongo docker")
+        logging.error(e)
+
+
+def bytes_to_MB(bytes):
+    return int(bytes / 1048576)
+
+
+def load_config():
+    logging.info("Loading config...")
+    global config
+    try:
+        file = open("config.json", "r")
+        config_str = file.read()
+        config = json.loads(config_str)
+        file.close()
+        return True
+    except Exception as e:
+        logging.info("Error in loading config")
+        logging.error(e)
+        return False
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    args = sys.argv
-    # setup_cron_job()
-    # disable_job()
-    # check_job()
-    globals()[args[1]](*args[2:])
+    logging.basicConfig(filename='psu.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+    check_memory()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
